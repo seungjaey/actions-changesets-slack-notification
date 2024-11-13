37ccdaa328fc1e0e9c27766afcc4c0846e1c2124
seungjaey <seungjaey@gmail.com>
2024-11-13 15:10:30 +0900

    init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
+import { IncomingWebhook } from '@slack/webhook'
+import { z } from 'zod'
+import { nanoid } from 'nanoid'
+import { isEmpty } from '@fxts/core'
 import * as core from '@actions/core'
-import { wait } from './wait'
+
+const MessageScheme = z.object({
+  name: z.string(),
+  version: z.string()
+})
+
+const MessagesScheme = z.array(MessageScheme)
+
+type Messages = z.infer<typeof MessagesScheme>
+
+const parseMessage = (message: string): Messages => {
+  try {
+    const json = JSON.parse(message)
+    return MessagesScheme.parse(json)
+  } catch (error) {
+    return []
+  }
+}
 
 /**
  * The main function for the action.
@@ -7,20 +28,79 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const WEB_HOOK_URL: string = core.getInput('WEB_HOOK_URL')
+    const OWNER: string = core.getInput('OWNER')
+    const AUTHOR: string = core.getInput('AUTHOR')
+    const GITHUB_URL: string = core.getInput('GITHUB_URL')
+    const REPOSITORY_NAME: string = core.getInput('REPOSITORY_NAME')
+    const CHANGESET_MESSAGE: string = core.getInput('CHANGESET_MESSAGE')
+    const webhook = new IncomingWebhook(WEB_HOOK_URL)
+    const messages = parseMessage(CHANGESET_MESSAGE)
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    if (isEmpty(messages)) {
+      return
+    }
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    await webhook.send({
+      blocks: [
+        {
+          type: 'header',
+          text: {
+            type: 'plain_text',
+            text: '📦 패키지 배포 알림',
+            emoji: true
+          }
+        }
+      ],
+      attachments: [
+        {
+          color: '#2EB67D',
+          blocks: [
+            {
+              type: 'section',
+              fields: [
+                {
+                  type: 'mrkdwn',
+                  text: '*Repository:*'
+                },
+                {
+                  type: 'mrkdwn',
+                  text: `*<${GITHUB_URL}/${OWNER}/${REPOSITORY_NAME}|${REPOSITORY_NAME}>*`
+                },
+                {
+                  type: 'mrkdwn',
+                  text: '*Author:*'
+                },
+                {
+                  type: 'mrkdwn',
+                  text: `*${AUTHOR}*`
+                }
+              ]
+            },
+            ...messages.map(({ name, version }) => {
+              return {
+                type: 'section',
+                text: {
+                  type: 'mrkdwn',
+                  text: `${name}@${version}`
+                },
+                accessory: {
+                  type: 'button',
+                  text: {
+                    type: 'plain_text',
+                    text: '링크',
+                    emoji: true
+                  },
+                  url: `${GITHUB_URL}/${OWNER}/${REPOSITORY_NAME}/pkgs/npm/${name.replace(`@${OWNER}\/`, '')}`,
+                  action_id: nanoid()
+                }
+              }
+            })
+          ]
+        }
+      ]
+    })
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
